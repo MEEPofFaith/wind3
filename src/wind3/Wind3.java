fdbea1e92c7cad65e1b6f3f5c1b01b7cb984b15d
MEEP of Faith <meepmatthew@gmail.com>
2023-01-31 23:00:02 -0800

    wind3

--- src/wind3/Wind3.java
+++ src/wind3/Wind3.java
@@ -1,18 +1,29 @@
 package wind3;
 
 import arc.*;
-import arc.util.*;
+import arc.audio.*;
+import arc.files.*;
+import arc.struct.*;
+import mindustry.*;
 import mindustry.game.EventType.*;
 import mindustry.mod.*;
-import mindustry.ui.dialogs.*;
+
+import static mindustry.Vars.*;
 
 public class Wind3 extends Mod{
 
     public Wind3(){
+        if(!headless){
+            Events.on(ClientLoadEvent.class, e -> {
+                Seq<Sound> sounds = new Seq<>();
+                Core.assets.getAll(Sound.class, sounds);
 
+                Fi wind3 = Vars.tree.get("sounds/wind3.ogg");
+                sounds.each(sound -> sound.load(wind3));
+            });
+        }
     }
 
     @Override
-    public void loadContent(){
-    }
+    public void loadContent(){}
 }
